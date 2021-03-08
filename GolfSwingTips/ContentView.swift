d68654b923530c7c10b7189956161ff010060354
Andre Robillard <andrerobillard@icloud.com>
2021-03-08 15:38:49 -0500

    Initial Commit

---
+++ GolfSwingTips/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GolfSwingTips
+//
+//  Created by Andre Robillard on 2021-03-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GolfSwingTips/GolfSwingTipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfSwingTipsApp.swift
+//  GolfSwingTips
+//
+//  Created by Andre Robillard on 2021-03-08.
+//
+
+import SwiftUI
+
+@main
+struct GolfSwingTipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
